25f98384eed19c6b47bf190f0cb33c3d398e1c14
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2024-01-19 08:51:01 -0300

    Refactor all test. (#15)

---
+++ tests/BootstrapPluginAssetTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\Asset\Tests;
+
+use PHPUnit\Framework\Attributes\RequiresPhp;
+use Yii\Asset\BootstrapPluginAsset;
+use Yii\Asset\Tests\Support\TestTrait;
+
+use function runkit_constant_redefine;
+
+final class BootstrapPluginAssetTest extends \PHPUnit\Framework\TestCase
+{
+    use TestTrait;
+
+    public function testRegister(): void
+    {
+        $this->assertFalse($this->assetManager->isRegisteredBundle(BootstrapPluginAsset::class));
+
+        $this->assetManager->register(BootstrapPluginAsset::class);
+
+        $this->assertSame(
+            ['/55145ba9/bootstrap.css' => ['/55145ba9/bootstrap.css']],
+            $this->assetManager->getCssFiles()
+        );
+        $this->assertFileExists(__DIR__ . '/Support/runtime/55145ba9/bootstrap.css');
+        $this->assertFileExists(__DIR__ . '/Support/runtime/55145ba9/bootstrap.css.map');
+        $this->assertFileDoesNotExist(__DIR__ . '/Support/runtime/55145ba9/bootstrap.min.css');
+        $this->assertFileDoesNotExist(__DIR__ . '/Support/runtime/55145ba9/bootstrap.min.css.map');
+
+        $this->assertSame(
+            ['/16b8de20/bootstrap.bundle.js' => ['/16b8de20/bootstrap.bundle.js']],
+            $this->assetManager->getJsFiles()
+        );
+        $this->assertFileExists(__DIR__ . '/Support/runtime/16b8de20/bootstrap.bundle.js');
+        $this->assertFileExists(__DIR__ . '/Support/runtime/16b8de20/bootstrap.bundle.js.map');
+        $this->assertFileDoesNotExist(__DIR__ . '/Support/runtime/16b8de20/bootstrap.bundle.min.js');
+        $this->assertFileDoesNotExist(__DIR__ . '/Support/runtime/16b8de20/bootstrap.bundle.min.js.map');
+    }
+
+    #[RequiresPhp('8.1')]
+    public function testRegisterWithEnvironmentProd(): void
+    {
+        @runkit_constant_redefine('YII_ENV', 'prod');
+
+        $this->assertFalse($this->assetManager->isRegisteredBundle(BootstrapPluginAsset::class));
+
+        $this->assetManager->register(BootstrapPluginAsset::class);
+
+        $this->assertSame(
+            ['/55145ba9/bootstrap.min.css' => ['/55145ba9/bootstrap.min.css']],
+            $this->assetManager->getCssFiles()
+        );
+        $this->assertFileExists(__DIR__ . '/Support/runtime/55145ba9/bootstrap.min.css');
+        $this->assertFileExists(__DIR__ . '/Support/runtime/55145ba9/bootstrap.min.css.map');
+        $this->assertSame(
+            ['/16b8de20/bootstrap.bundle.min.js' => ['/16b8de20/bootstrap.bundle.min.js']],
+            $this->assetManager->getJsFiles()
+        );
+        $this->assertFileExists(__DIR__ . '/Support/runtime/16b8de20/bootstrap.bundle.min.js');
+        $this->assertFileExists(__DIR__ . '/Support/runtime/16b8de20/bootstrap.bundle.min.js.map');
+
+        @runkit_constant_redefine('YII_ENV', 'test');
+    }
+}
---
+++ tests/BootstrapPluginCdnAssetTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\Asset\Tests;
+
+use Yii\Asset\BootstrapPluginCdnAsset;
+use Yii\Asset\Tests\Support\TestTrait;
+
+final class BootstrapPluginCdnAssetTest extends \PHPUnit\Framework\TestCase
+{
+    use TestTrait;
+
+    public function testRegister(): void
+    {
+        $this->assertFalse($this->assetManager->isRegisteredBundle(BootstrapPluginCdnAsset::class));
+
+        $this->assetManager->register(BootstrapPluginCdnAsset::class);
+
+        $this->assertSame(
+            [
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' => [
+                    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+                    'crossorigin' => 'anonymous',
+                    'integrity' => 'sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN',
+                    'rel' => 'stylesheet',
+                ],
+            ],
+            $this->assetManager->getCssFiles()
+        );
+        $this->assertSame(
+            [
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js' => [
+                    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+                    'crossorigin' => 'anonymous',
+                    'integrity' => 'sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL',
+                ],
+            ],
+            $this->assetManager->getJsFiles()
+        );
+    }
+}
